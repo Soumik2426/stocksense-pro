0ac34111cf60d1d547b03f77c827225d02309c5e
Soumik2426 <soumikmaity2426@gmail.com>
2026-05-10 19:44:56 +0530

    Added the Dead letter topic

--- backend/inventory-service/inventory-service/src/main/java/com/stocksense/inventoryservice/kafka/KafkaTopicConfig.java
+++ backend/inventory-service/inventory-service/src/main/java/com/stocksense/inventoryservice/kafka/KafkaTopicConfig.java
@@ -3,6 +3,7 @@ package com.stocksense.inventoryservice.kafka;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaTopicConfig {
@@ -15,5 +16,13 @@ public class KafkaTopicConfig {
                 1,
                 (short) 1
         );
+    }
+
+    @Bean
+    public NewTopic deadLetterTopic() {
+
+        return TopicBuilder.name(
+                "inventory-events-dlt"
+        ).build();
     }
 }
